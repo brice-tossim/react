8cde6ed2f51de03c0cfd280367d8c63f59e79b71
brice-tossim <tossimbrice@gmail.com>
2025-07-08 19:30:10 -0600

    Add default value for appwrite config

--- 2-movie-app/src/config/appwrite.config.ts
+++ 2-movie-app/src/config/appwrite.config.ts
@@ -1,9 +1,9 @@
 import { Client, Databases } from "appwrite";
 
-const ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
-const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
-const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
-const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT ?? "";
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID ?? "";
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID ?? "";
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID ?? "";
 
 // Initialize Appwrite Client
 const client = new Client().setEndpoint(ENDPOINT).setProject(PROJECT_ID);
